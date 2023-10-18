a88f41ff87c85ecb7c9f7d169a7d3da3848a4249
marlonchiu <Knight1898@outlook.com>
2023-10-18 15:06:51 +0800

    引入 tailwindcss

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,5 +1,9 @@
 function Chat() {
-  return <div className="chat">chat</div>
+  return (
+    <div className="chat">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  )
 }
 
 export default Chat
